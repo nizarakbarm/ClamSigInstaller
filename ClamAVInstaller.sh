59dc51789f385a5d9592bd11c1111c029933ee17
nizarakbarm <nizarakbarm04@gmail.com>
2024-01-23 13:57:58 +0700

    feat(ClamAVInstaller): check if html-xml-utils is installed

--- ClamAVInstaller.sh
+++ ClamAVInstaller.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 #Get latest LTS
+if [ ! -f "/usr/bin/html-xml-utils" ]; then
+    case $(grep -E '^(NAME)=' /etc/os-release | cut -d"=" -f2 | tr -d '"') in
+        Ubuntu|Debian)
+            apt install -y html-xml-utils
+            ;;
+        Centos)
+            yum install -y html-xml-utils
+            ;;
+        *)
+            echo -n "Unknown error!"
+            exit 1
+            ;;
+    esac
+fi
 latestlts_release=$(curl -s -A @ua https://www.clamav.net/downloads -L | grep '<span class="badge">LTS</span>' | hxselect -c -s "\n" "h4" | grep -oE "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+" | head -n 1)
 
 #Install ClamAV for Ubuntu and Debian
@@ -34,7 +48,7 @@ install_clamav_centos() {
 }
 
 #Download the package based on the OS release
-case $(grep -E '^(NAME)=' /etc/os-release) in
+case $(grep -E '^(NAME)=' /etc/os-release | cut -d"=" -f2 | tr -d '"') in
     Ubuntu|Debian)
         install_clamav_ubuntu $latestlts_release
         ;;
